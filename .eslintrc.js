3eb1c78a360939757936b88fb8c734905be9f7de
Giles Dring <giles@dringtech.com>
2019-01-28 13:58:46 +0000

    Add rules to eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -5,8 +5,14 @@ module.exports = {
   },
   extends: 'standard',
   rules: {
-    semi: ['error', 'always'],
-    'comma-dangle': ['error', 'always-multiline'],
-    'no-console': 'error',
-  }
+    'semi': [ 'error', 'always' ],
+    'comma-dangle': [ 'error', 'always-multiline' ],
+    'no-console': 'warning',
+    'space-before-function-paren': {
+      'anonymous': 'always',
+      'named': 'never',
+      'asyncArrow': 'always',
+    },
+    'object-curly-newline': [ 'error', { 'multiline': true } ],
+  },
 };
